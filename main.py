326cc3e2f661b9c98b7207858ceefebceb81f064
Tyler <tyler.wright5146@gmail.com>
2023-03-04 12:15:43 -0700

    read in data

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+raw_poverty_data = pd.read_csv("data/poverty.csv")
+raw_human_dev_data = pd.read_csv("data/human_development.csv")
+raw_gender_inequality_data = pd.read_csv("gender_inequality.csv")
+raw_world_happiness_data = pd.read_csv("world_happiness.csv")
