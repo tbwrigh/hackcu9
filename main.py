9fa98d1460cef72b6286344b6a34e17065414016
Tyler <tyler.wright5146@gmail.com>
2023-03-04 12:56:24 -0700

    add data path

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 raw_poverty_data = pd.read_csv("data/poverty.csv")
 raw_human_dev_data = pd.read_csv("data/human_development.csv")
-raw_gender_inequality_data = pd.read_csv("gender_inequality.csv")
-raw_world_happiness_data = pd.read_csv("world_happiness.csv")
-raw_sexual_orientation_data = pd.read_csv("sexual_orientation.csv")
-raw_most_crowded_data = pd.read_csv("most_crowded.csv")
+raw_gender_inequality_data = pd.read_csv("data/gender_inequality.csv")
+raw_world_happiness_data = pd.read_csv("data/world_happiness.csv")
+raw_sexual_orientation_data = pd.read_csv("data/sexual_orientation.csv")
+raw_most_crowded_data = pd.read_csv("data/most_crowded.csv")
 
 pov_data = raw_poverty_data.dropna()
 hd_data = raw_human_dev_data.dropna()
