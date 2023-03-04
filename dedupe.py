c779c8ee740072a329d605c5e8ce0a1598a975bd
Tyler <tyler.wright5146@gmail.com>
2023-03-04 16:04:25 -0700

    datasets

--- dedupe.py
+++ dedupe.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
-raw_pop_data = pd.read_csv("data/gender-inequality-index.csv")
+raw_pop_data = pd.read_csv("lgbtq_inclusiveness.csv")
 
-pop_data = raw_pop_data[["Country", "gii"]].dropna()
+pop_data = raw_pop_data.dropna()
 
 print(len(pop_data["Country"].values.tolist()))
 print(len(set(pop_data["Country"].values.tolist())))
+
+c = set()
+for i in pop_data["Country"].values.tolist():
+    if pop_data["Country"].values.tolist().count(i) > 1:
+        c.add(i)
+
+print("\n".join(sorted(c)))
