42d4ff98af2f670aa2f3002a0a94c16d7e1dd3fd
Tyler <tyler.wright5146@gmail.com>
2023-03-04 12:54:20 -0700

    start code t oextract countries

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ raw_world_happiness_data = pd.read_csv("world_happiness.csv")
 raw_sexual_orientation_data = pd.read_csv("sexual_orientation.csv")
 raw_most_crowded_data = pd.read_csv("most_crowded.csv")
 
+pov_data = raw_poverty_data.dropna()
+hd_data = raw_human_dev_data.dropna()
+gi_data = raw_gender_inequality_data.dropna()
+wh_data = raw_world_happiness_data.dropna()
+so_data = raw_sexual_orientation_data.dropna()
+mc_data = raw_most_crowded_data.dropna()
+
+countries = set(pov_data["Region"].values.tolist())
+countries &= set(hd_data["Country"].values.tolist())
