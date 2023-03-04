18000b334a2d9b190e0d14b742ec8030e35e83cb
Tyler <tyler.wright5146@gmail.com>
2023-03-04 15:23:19 -0700

    dedupe

--- dedupe.py
+++ dedupe.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
-raw_pop_data = pd.read_csv("population.csv")
+raw_pop_data = pd.read_csv("data/gen_inequality.csv")
 
 print(len(raw_pop_data["Country"].values.tolist()))
 print(len(set(raw_pop_data["Country"].values.tolist())))
+
+for i in raw_pop_data["Country"].values.tolist():
+    if raw_pop_data["Country"].values.tolist().count(i) > 1:
+        print(i)
