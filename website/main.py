541dd0934c74c9eeb2e1bfdb6446b72257fe24ff
Tyler <tyler.wright5146@gmail.com>
2023-03-04 20:19:08 -0700

    serve css

--- website/main.py
+++ website/main.py
@@ -8,6 +8,10 @@ async def read_root():
     # Return an HTML file as a download response
     return FileResponse("html/home.html")
 
+@app.get("/slider_design.css")
+async def silder_css():
+    return FileResponse("html/style.css")
+
 @app.post("/find")
 async def calculate_results(gender_identity: str = Form(), sexual_orientation: str = Form(), social: str = Form(), wealth: str = Form(), religion: str = Form()):
     
