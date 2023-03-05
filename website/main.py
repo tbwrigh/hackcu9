57ed5baa243bb168fe8f1894a19d3adfc5bc24ad
Tyler <tyler.wright5146@gmail.com>
2023-03-04 18:03:05 -0700

    stuf

--- website/main.py
+++ website/main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 async def read_root():
     # Return an HTML file as a download response
     return FileResponse("html/home.html")
+
+@app.post("/find")
+async def calculate_results(gender_identity: str = Form(), sexual_orientation: str = Form(), social: str = Form(), wealth: str = Form(), religion: str = Form()):
+    
+    return RedirectResponse("/")
